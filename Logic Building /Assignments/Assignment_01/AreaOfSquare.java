d7cb2aaddc0e196fd66b228d36a27790db88bae5
riteshdesaicmaug26 <ritesh.desai.cmaug26@gmail.com>
2026-08-18 16:39:14 +0530

    Delete Logic Building /Assignments/Assignment_01 directory

--- Logic Building /Assignments/Assignment_01/AreaOfSquare.java
+++
@@ -1,9 +0,0 @@
-public class AreaOfSquare {
-  public static void main(String[] args) {
-
-   int Length = 4;
-   int Area = Length*Length;
-
-   System.out.println("Area of Square is " + Area);
- }
-}

9bb410e642552f49a819027d26d67b036a6114f5
riteshdesaicmaug26 <ritesh.desai.cmaug26@gmail.com>
2026-09-16 22:44:57 +0530

    Create Program3.java

---
+++ OOPJ Assignments/Assignment_4(Inheritence)/Q4/Program3.java
@@ -0,0 +1,18 @@
+
+public class Program3 {
+
+	public static void main(String[] args) {
+
+		Square2 obj = new Square2();
+		obj.displayShape();
+		obj.displayRectangle();
+		
+		
+		// extra for practise (not part of assignment
+//		Shape obj1 = new Square2();   //Upcasting; storing a child object in a parent reference.		
+//		Square2 obj2 = (Square2)obj1; //Downcasting; giving childs access to parent class
+		
+//		obj2.displaySquareIsRectangle();
+	}
+
+}

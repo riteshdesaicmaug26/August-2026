7852a919281beae3f5313d901e6e6ccec00e0fe7
riteshdesaicmaug26 <ritesh.desai.cmaug26@gmail.com>
2026-09-16 22:20:44 +0530

    Create Program.java

---
+++ OOPJ Assignments/Assignment_3(For Loops)/Program.java
@@ -0,0 +1,17 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+//		System.out.println("Choose Operation Using For Loop: ");
+
+		DisplayNumber Number = new DisplayNumber();
+		Number.evenNumber();
+		Number.oddNumber();
+		Number.seventhNumber();
+		Number.patternTwo();
+		Number.patternThree();
+		Number.patternFour();
+		Number.patternFive();
+	}
+}

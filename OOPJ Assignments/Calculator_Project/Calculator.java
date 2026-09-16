e1042d4540b55a5b5bd40b802dd6ee4f08a36099
riteshdesaicmaug26 <ritesh.desai.cmaug26@gmail.com>
2026-09-16 21:55:48 +0530

    Create Calculator.java

---
+++ OOPJ Assignments/Calculator_Project/Calculator.java
@@ -0,0 +1,23 @@
+public class Calculator{
+	public int add(int num1, int num2)
+	{
+		return num1 + num2;
+	}	
+	public float add(float num1, float num2)
+	{
+		return num1 + num2;
+	}
+	public float sub(float num1, float num2)
+	{
+		return num1 - num2;
+	}
+	public float mul(float num1, float num2)
+	{
+		return num1 * num2;
+	}
+	public float div(float num1, float num2)
+	{
+		return num1 / num2;
+	}
+	
+}

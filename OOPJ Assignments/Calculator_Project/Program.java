5671648c8c4080a305fc0553c214ba7477f82d00
riteshdesaicmaug26 <ritesh.desai.cmaug26@gmail.com>
2026-09-16 21:56:21 +0530

    Create Program.java

---
+++ OOPJ Assignments/Calculator_Project/Program.java
@@ -0,0 +1,20 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Enter the first number: ");
+		float num1 = ConsoleInput.getFloat();
+		
+		System.out.println("Enter the second number: ");
+		float num2 = ConsoleInput.getFloat();
+		
+		Calculator objCalculator = new Calculator();
+		
+		float result = objCalculator.mul(num1, num2);
+		
+		System.out.println(result);
+		
+		
+	}
+}

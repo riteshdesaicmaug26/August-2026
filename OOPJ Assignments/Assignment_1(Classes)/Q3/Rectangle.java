1ba5918909bcfeec4b1196bd407a3862ae05fcc1
riteshdesaicmaug26 <ritesh.desai.cmaug26@gmail.com>
2026-09-16 22:02:35 +0530

    Create Rectangle.java

---
+++ OOPJ Assignments/Assignment_1(Classes)/Q3/Rectangle.java
@@ -0,0 +1,24 @@
+
+public class Rectangle {
+	
+	int length;
+	int breadth;
+	
+	Rectangle(int length, int breadth)
+	{
+        this.length = length;
+        this.breadth = breadth;
+	}
+
+	 int area(){
+		return length * breadth;
+	}
+	 
+	 public static void main(String[] args) {
+		 Rectangle r1 = new Rectangle(4,5);
+		 Rectangle r2 = new Rectangle(5,8);
+		 
+		 System.out.println("Area of rectangle 1: " + r1.area());
+		 System.out.println("Area of rectangle 1: " + r2.area());
+	 }
+}
